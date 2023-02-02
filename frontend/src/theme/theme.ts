a440603d1528222d083606f0f08fb1308106d370
double-hope <nadia.prohorchuk@gmail.com>
2023-02-02 11:04:49 +0200

    feature: add theme styles

--- frontend/src/theme/theme.ts
+++ frontend/src/theme/theme.ts
@@ -1,13 +1,16 @@
 const colors = {
     background: '#FFFFFF',
+    light: '#FFFFFF',
+    lightHover: '#F1F1F1',
     text: '#000000',
-    accent: '#B03F00'
+    accent: '#B03F00',
+    accentHover: '#9F3900'
 };
 
 const fontSizes = {
   h1: '180px',
-  body: '25px',
-  button: '30px',
+  body: '16px',
+  button: '18px',
   caption: '12px',
 };
 
@@ -49,7 +52,7 @@ const radiuses = {
   xs: '5px',
   sm: '7px',
   md: '10px',
-  lg: '50px',
+  lg: '24px',
   circle: '50%',
   cardTub: '15px',
 };
@@ -63,6 +66,12 @@ const shadows = {
 
 const borders = {
   dropdown: `10px solid ${colors.background}`,
+};
+
+const opacities = {
+  lg: 0.2,
+  md: 0.4,
+  sm: 0.6,
 };
 
 const zIndex = {
@@ -104,6 +113,7 @@ const lightTheme = {
   mq,
   maxMq,
   zIndex,
+  opacities,
 };
 
 export { lightTheme };
