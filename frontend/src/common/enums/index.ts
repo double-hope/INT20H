c436494488f1ee3d86f5ba4989c6c04d594242e5
double-hope <nadia.prohorchuk@gmail.com>
2023-02-02 15:20:05 +0200

    add input enum

--- frontend/src/common/enums/index.ts
+++ frontend/src/common/enums/index.ts
@@ -1,2 +1,3 @@
 export * from './button';
 export * from './background';
+export * from './input';
---
+++ frontend/src/common/enums/input.ts
@@ -0,0 +1,5 @@
+export enum InputEnum {
+    TEXT = 'text',
+    EMAIL = 'email',
+    PASSWORD = 'password'
+}
